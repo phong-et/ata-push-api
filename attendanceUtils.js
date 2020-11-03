9ca2117bfd4e4d12c2544b4a74a4d3008f4bc2bd
Phillip <phillip7.et@gmail.com>
2020-11-03 13:56:34 +0700

    hard code office hours to test heroku app

--- attendanceUtils.js
+++ attendanceUtils.js
@@ -29,9 +29,19 @@ function notify(type) {
 }
 
 async function fetchOfficeSettings() {
-  return JSON.parse(
-    await (await fetch(hostAta + '/api/officesettings')).text()
-  );
+  // return JSON.parse(
+  //   await (await fetch(hostAta + '/api/officesettings')).text()
+  // );
+  return {
+    lazyLoader: {},
+    id: '32274e29-84cb-403d-9e11-08d8354337cc',
+    ipAddress: '42.119.146.86',
+    latitude: 10.743219786522866,
+    longitude: 106.70158412307501,
+    authRange: 150,
+    startTime: '2000-01-01T02:30:00+00:00',
+    endTime: '2000-01-01T18:00:00+07:00',
+  };
 }
 
 function formatMH(mhNumber) {
