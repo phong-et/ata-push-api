612fd6701925c1ba897070a9cc0b6ebbab46b19f
Phillip <phillip7.et@gmail.com>
2021-03-26 19:19:28 +0700

    fix file name

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (_, res, _) {
-  res.sendFile('index2.html');
+  res.sendFile('index.html');
 });
 
 module.exports = router;
